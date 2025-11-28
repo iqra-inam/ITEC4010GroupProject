d96b77676e0a901d34bd55488e98ad2b042d6484
mustafa Salman <mustafasalman969@gmail.com>
2025-11-28 01:57:40 -0500

    completed business side

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Itecproject@123",
+    password: "",
     database: "queue_system"
 });
 
